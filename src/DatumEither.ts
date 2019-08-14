91de0338296e17bbaa87339c0da4d45aa4b92ff0
Brandon Blaylock <brandon@null.pub>
2019-08-13 17:47:44 -0700

    feat: added more documentation and helper functions

--- src/DatumEither.ts
+++ src/DatumEither.ts
@@ -1,9 +1,41 @@
-import { Either } from 'fp-ts/es6/Either';
+/**
+ * @file Represents a value of one of six possible types (a disjoint union).
+ *
+ * An instance of `DatumEither` is equivalent to `Datum<Either<E, A>>`
+ *
+ * A common use of `DatumEither` is as a container for dealing with refreshable data values that
+ * can have error conditions. The full type list is:
+ *
+ * `Initial`
+ * `Pending`
+ * `Refresh<Either<E, A>>`
+ *   `Refresh<Left<E>>`
+ *   `Refresh<Right<A>>`
+ * `Replete<Either<E, A>>`
+ *   `Replete<Left<E>>`
+ *   `Replete<Right<A>>`
+ *
+ * There are additional helper methods for going from refresh to replete and back.
+ */
+
+import { Either, left, isRight, Right, isLeft, Left } from 'fp-ts/es6/Either';
 import { EitherM1, getEitherM } from 'fp-ts/es6/EitherT';
 import { Monad2 } from 'fp-ts/es6/Monad';
 import { pipeable } from 'fp-ts/es6/pipeable';
 
-import { datum, Datum, URI as DatumURI } from './Datum';
+import {
+  datum,
+  Datum,
+  fold,
+  URI as DatumURI,
+  refresh,
+  replete,
+  isReplete,
+  Replete,
+  constPending,
+} from './Datum';
+
+export { initial, pending, constInitial, constPending } from './Datum';
 
 /**
  * A Monad instance for `Datum<Either<E, A>>`
@@ -27,12 +59,79 @@ export const URI = '@nll/async-data/datum-either';
 export type URI = typeof URI;
 
 /**
+ * 2.1.0
+ */
+export type DatumEither<E, A> = Datum<Either<E, A>>;
+
+/**
  * @since 2.0.0
  */
 export const datumEither: Monad2<URI> & EitherM1<DatumURI> = {
   ...getEitherM(datum),
   URI,
 };
+
+/**
+ * @since 2.1.0
+ */
+export const success = <A>(a: A) => datumEither.of(a);
+
+/**
+ * @since 2.1.0
+ */
+export const failure = <E>(e: E) => replete(left(e));
+
+/**
+ * @since 2.1.0
+ */
+export const isSuccess = <E, A>(
+  fea: DatumEither<E, A>
+): fea is Replete<Right<A>> => isReplete(fea) && isRight(fea.value);
+
+/**
+ * @since 2.1.0
+ */
+export const isFailure = <E, A>(
+  fea: DatumEither<E, A>
+): fea is Replete<Left<E>> => isReplete(fea) && isLeft(fea.value);
+
+/**
+ * @since 2.1.0
+ */
+export const toRefresh = <E, A>(fea: DatumEither<E, A>): DatumEither<E, A> =>
+  fold<Either<E, A>, DatumEither<E, A>>(
+    constPending,
+    constPending,
+    () => fea,
+    a => refresh(a)
+  )(fea);
+
+/**
+ * @since 2.1.0
+ */
+export const refreshFold = <E, A, B>(
+  onInitial: () => B,
+  onPending: () => B,
+  onFailure: (e: E, r?: boolean) => B,
+  onSuccess: (a: A, r?: boolean) => B
+) => (fea: DatumEither<E, A>): B =>
+  fold<Either<E, A>, B>(
+    onInitial,
+    onPending,
+    e => (isRight(e) ? onSuccess(e.right, true) : onFailure(e.left, true)),
+    e => (isRight(e) ? onSuccess(e.right, false) : onFailure(e.left, false))
+  )(fea);
+
+/**
+ * @since 2.1.0
+ */
+export const refreshFoldR = <E, A, B>(
+  fea: DatumEither<E, A>,
+  onInitial: () => B,
+  onPending: () => B,
+  onFailure: (e: E, r?: boolean) => B,
+  onSuccess: (a: A, r?: boolean) => B
+): B => refreshFold(onInitial, onPending, onFailure, onSuccess)(fea);
 
 const {
   alt,
