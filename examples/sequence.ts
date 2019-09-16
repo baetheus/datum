2c795f4c1378cf90502bdf9c8d1e3706d6c7db6c
Brandon Blaylock <brandon@null.pub>
2019-09-16 15:25:00 -0700

    chore: added a better sequence example

---
+++ examples/sequence.ts
@@ -0,0 +1,46 @@
+import { sequenceT } from 'fp-ts/lib/Apply';
+import { pipe } from 'fp-ts/lib/pipeable';
+
+import { DatumEither, datumEither, failure, initial, map, pending, success, toRefresh } from '../src/DatumEither';
+
+const sequence = sequenceT(datumEither);
+
+// Here are some DatumEithers
+const someInitial: DatumEither<string, string> = initial; // Type: Initial
+const somePending: DatumEither<string, string> = pending; // Type: Pending
+const someSuccess: DatumEither<string, string> = success('DATA!'); // Type: Replete<Right<string>>
+const someFailure: DatumEither<string, string> = failure('ERROR!'); // Type: Replete<Left<string>>
+
+// Here are some Refreshing Datume
+const someRefreshingSuccess = toRefresh(someSuccess); // Type: Refresh<Right<string>>
+const someRefreshingFailure = toRefresh(someFailure); // Type: Refresh<Left<string>>
+
+const result1 = pipe(
+  sequence(someInitial, somePending, someSuccess),
+  map(([a, b, c]) => a + b + c)
+);
+console.log(result1);
+/**
+ * The initial short circuits the sequence
+ * result1 === initial
+ */
+
+const result2 = pipe(
+  sequence(someRefreshingSuccess, someRefreshingFailure),
+  map(([a, b]) => a + b)
+);
+console.log(result2);
+/**
+ * The failure shortcircuits the sequence.
+ * result2 === refresh(left('ERROR!'))
+ */
+
+const result3 = pipe(
+  sequence(someSuccess, someRefreshingSuccess),
+  map(([a, b]) => a + b)
+);
+console.log(result3);
+/**
+ * All values are good, so map is called, since one of the DatumEithers is refreshing, the sequence is refreshing
+ * result3 = refresh(right('DATA!DATA!'))
+ */
