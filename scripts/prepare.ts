3aaf2b4c14c9db36cf85bf7f1a5124f160789ca6
Brandon Blaylock <brandon@null.pub>
2020-03-11 16:15:55 -0700

    chore: copy src and update prepare to fix sourcemaps

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -1,11 +1,12 @@
 ///<reference path="../node_modules/@types/node/index.d.ts" />
 import * as fs from 'fs';
+import * as path from 'path';
 
 /**
  * Treat package.json root as root.
  */
-const buildDir = './dist';
-const copyFiles = ['README.md'];
+const outDir = './dist';
+const copyItems = ['README.md', 'src']; // For Sourcemaps
 
 const pkg = require('../package.json');
 
@@ -24,15 +25,57 @@ const publishPackage = {
   dependencies: pkg.dependencies
 };
 
-if (!fs.existsSync(buildDir)) {
-  fs.mkdirSync(buildDir);
+const copyRecursiveSync = (src: string, dest: string): void => {
+  const srcIsDirectory = fs.statSync(src).isDirectory();
+  if (srcIsDirectory) {
+    if (!fs.statSync(dest).isDirectory()) {
+      fs.mkdirSync(dest);
+    }
+    fs.readdirSync(src).forEach(function(childItemName) {
+      copyRecursiveSync(
+        path.join(src, childItemName),
+        path.join(dest, childItemName)
+      );
+    });
+  } else {
+    fs.copyFileSync(src, dest); // UPDATE FROM:    fs.linkSync(src, dest);
+  }
+};
+
+const remapSources = (map: { sources: string[] }) => {
+  const sources = map.sources.map(source =>
+    source.replace(/\.\.\/src/, './src')
+  );
+  return {
+    ...map,
+    sources
+  };
+};
+
+if (!fs.existsSync(outDir)) {
+  fs.mkdirSync(outDir);
 }
 
+// Write package.json
 fs.writeFileSync(
-  [buildDir, 'package.json'].join('/'),
+  [outDir, 'package.json'].join('/'),
   JSON.stringify(publishPackage, null, 2),
   {
     encoding: 'utf-8'
   }
 );
-copyFiles.forEach(file => fs.copyFileSync(file, [buildDir, file].join('/')));
+
+// Copy additional items
+copyItems.forEach(src => copyRecursiveSync(src, [outDir, src].join('/')));
+
+// Modify sourcemaps
+const sourceMaps = fs
+  .readdirSync(outDir)
+  .filter(file => file.endsWith('js.map'))
+  .map(file => [outDir, file].join('/'));
+
+sourceMaps.forEach(map => {
+  const file = JSON.parse(fs.readFileSync(map, { encoding: 'utf-8' }));
+  const newFile = remapSources(file);
+  fs.writeFileSync(map, JSON.stringify(newFile), { encoding: 'utf-8' });
+});
