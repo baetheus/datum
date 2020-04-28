263644f1f9284e5f9712409a38afe66c0f419e73
alex-ketch <alex@ketch.me>
2020-04-28 10:25:17 -0400

    feat: Re-export modules from a main index file

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * @since 3.1.0
+ */
+
+import * as datum from './Datum';
+import * as datumEither from './DatumEither';
+
+export {
+  /**
+   * @since 3.1.0
+   */
+  datum,
+  /**
+   * @since 3.1.0
+   */
+  datumEither
+};
